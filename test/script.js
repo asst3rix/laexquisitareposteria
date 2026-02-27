a8d310deb1e624d5e4b44cd02cbcb366bee94000
asst3rix_WC <185900998+asst3rix@users.noreply.github.com>
2026-02-27 14:57:29 +0100

    Change how the burger icon is displayed. Create an animation when clicking the burger icon.

--- test/script.js
+++ test/script.js
@@ -1,10 +1,10 @@
 const burgerMenuManagement = (function () {
     // When we click the burger menu, we show the menu and change the icon to a cross.
-    const burger = document.getElementById('burger');
+    const burger = document.querySelector('#burger-container');
     const navLinks = document.getElementById('nav-links');
     burger.addEventListener('click', () => {
         navLinks.classList.toggle('active');
-        burger.classList.toggle('active-burger');
+        burger.classList.toggle('clicked');
     });
 
     // We're doing this to remove the burger menu after clicking on a link and set the icon back as a burger.
@@ -12,7 +12,7 @@ const burgerMenuManagement = (function () {
     links.forEach(link => {
         link.addEventListener('click', () => {
             navLinks.classList.remove('active');
-            burger.classList.remove('active-burger');
+            burger.classList.remove('clicked');
         });
     });
 })();
@@ -50,7 +50,7 @@ const thumbnailsManagement = (function () {
         // We give him where to observe.
         root: mainImageContainer,
         // Image is consider active when 100% of its surface is visible.
-        threshold: 1  
+        threshold: 1
     };
 
     // We give to IntersectionObserver, 1st the what to do (the function), 2nd what to use (observerOptions)
